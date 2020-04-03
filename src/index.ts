13e393838afeb3fa5cf8e65bf46f9564a9018807
taoqf <tao_qiufeng@126.com>
2020-04-03 08:56:08 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,36 @@
-export default function fun() {
+
+export default function show_toast(title: string, icon: 'success' | 'loading' | 'none', duration?: number, image?: string, mask?: boolean) {
+	const options = {};
+
+	if (duration) {
+		Object.assign(options, {
+			duration
+		});
+	}
+	if (image !== undefined) {
+		Object.assign(options, {
+			image
+		});
+	}
+	if (mask !== undefined) {
+		Object.assign(options, {
+			mask
+		});
+	}
+	return new Promise<boolean>((resolve) => {
+		wx.showToast({
+			// duration,
+			// image,
+			icon,
+			// mask,				// 是否显示透明蒙层，防止触摸穿透，默认：false
+			title,
+			success() {
+				resolve(true);
+			},
+			fail() {
+				resolve(false);
+			},
+			...options
+		});
+	});
 }
